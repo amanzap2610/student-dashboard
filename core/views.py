1953fc34858da1b644d0b1d5fb27b8bca057816d
amansaxena.zapbuild@gmail.com <amansaxena.zapbuild@gmail.com>
2020-05-19 03:55:34 +0530

    login done

---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+from core.forms import SignUpForm, ProfileForm
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+# Sign Up View
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'commons/signup.html'
+
+# Edit Profile View
+class ProfileView(UpdateView):
+    model = User
+    form_class = ProfileForm
+    success_url = reverse_lazy('home')
+    template_name = 'commons/profile.html'
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = "home.html"
